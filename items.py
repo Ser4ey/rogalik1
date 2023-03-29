d574afef0ee1847a5811046cdfe98347b8266d41
Gleb <m_marple1@mail.ru>
2023-03-29 19:23:35 +1000

    added the ability to equip armor and weapons, a method to restore health, and the hero's connection with objects

--- items.py
+++ items.py
@@ -4,37 +4,42 @@ class Item:
         self.description = description
 
 class Armor(Item):
-    def __init__(self, name, description, item_armor):
+    def __init__(self, name, description, item_armor, price):
         super().__init__(name, description)
         self.item_armor = item_armor
+        self.price = price
 
 class Weapon(Item):
-    def __init__(self, name, description, item_damage):
+    def __init__(self, name, description, item_damage, price):
         super().__init__(name, description)
         self.item_damage = item_damage
+        self.price = price
 
 class Potion(Item):
-    def __init__(self, name, description, duration):
+    def __init__(self, name, description, duration, price):
         super().__init__(name, description)
         self.duration = duration
+        self.price = price
 
 class Healing_Potion(Potion):
-    def __init__(self, name, description, duration):
+    def __init__(self, name, description, duration, price):
         super().__init__(name, description, duration)
+        self.price = price
 
 
 class Damage_Potion(Potion):
-    def __init__(self, name, description, duration):
+    def __init__(self, name, description, duration, price):
         super().__init__(name, description, duration)
+        self.price = price
 
 
 
-Assault_cuirass = Armor("Штурмовая кираса", "Тяжелая броня, покрытая кроваво-красной краской", 30)
-Shivas_guard = Armor("Броня Шивы", "Изысканный доспех, сделанный из черного металла, украшенный кристаллами и ониксом", 20)
-Blade_mail = Armor("Кольчуга с лезвием", " Кираса из черной драконьей чешуи", 15)
+Assault_cuirass = Armor("Штурмовая кираса", "Тяжелая броня, покрытая кроваво-красной краской", 30, 50)
+Shivas_guard = Armor("Броня Шивы", "Изысканный доспех, сделанный из черного металла, украшенный кристаллами и ониксом", 20, 30)
+Blade_mail = Armor("Кольчуга с лезвием", " Кираса из черной драконьей чешуи", 15, 20)
 
-Blood_kings_blade = Weapon(" ", "Меч, пропитанный кровью врагов", 20)
-Fallen_angel_bow = Weapon("Лук Падшего Ангела", "Лук, который поражает душу врага", 18)
-Black_warrior_axe = Weapon("Топор Черного Воина", "Топор, насыщенный злом и жаждой убийств", 32)
+Blood_kings_blade = Weapon(" ", "Меч, пропитанный кровью врагов", 20, 10)
+Fallen_angel_bow = Weapon("Лук Падшего Ангела", "Лук, который поражает душу врага", 18, 9)
+Black_warrior_axe = Weapon("Топор Черного Воина", "Топор, насыщенный злом и жаждой убийств", 32, 20)
 
-Healing_potion = Potion("Зелье исцеления", "Это зелье восстанавливает немного здоровья, помогая вам пережить трудные битвы", 15)
+Healing_potion = Potion("Зелье исцеления", "Это зелье восстанавливает немного здоровья, помогая вам пережить трудные битвы", 15, 10)
